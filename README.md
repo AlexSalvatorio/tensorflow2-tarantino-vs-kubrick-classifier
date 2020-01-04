a884e6a77958fd7e49f3616751c0ff3d8244d7ff
Alexandre Salvatore <alexander.salvatore@gmail.com>
2020-01-04 19:44:16 +0100

    Try display diff

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Tensorflow 2.0 : Horror VS Western
 
-This Python script is an experimentation of mine to classify movie screen capture, making difference between Horror movies and Western movies. A big portion of the code come from Arun Prakash's [Build your own Image classifier with Tensorflow and Keras](https://blog.francium.tech/build-your-own-image-classifier-with-tensorflow-and-keras-dc147a15e38e), adapted for Tensorflow 2.0
+This Python script is an experimentation of mine to classify movie screen capture, making difference between Tarantino and Kubrick's movies. A big portion of the code come from Arun Prakash's [Build your own Image classifier with Tensorflow and Keras](https://blog.francium.tech/build-your-own-image-classifier-with-tensorflow-and-keras-dc147a15e38e), adapted for Tensorflow 2.0
 
 
 ## Author
--- train3.py
+++ train3.py
@@ -6,6 +6,8 @@
 import pathlib2
 import os
 import numpy as np
+from tqdm import tqdm
+import cv2
 
 import tensorflow as ts
 from tensorflow.keras.models import Sequential
@@ -25,6 +27,7 @@ print("Eager execution: {}".format(ts.executing_eagerly()))
 
 train_dir = '../../../Trainning/training-movie-director/train'
 validation_dir = '../../../Trainning/training-movie-director/validation'
+test_dir = '../../../Trainning/training-movie-director/test'
 types = ('*/*.png', '*/*.gif','*/*.jpg')
 
 
@@ -71,7 +74,8 @@ BATCH_SIZE = 32
 IMG_HEIGHT = 100
 IMG_WIDTH = 239
 #STEPS_PER_EPOCH = np.ceil(image_count/BATCH_SIZE)
-EPOCHS = 17
+EPOCHS = 15
+#EPOCHS = 1
 
 train_data_gen = train_image_generator.flow_from_directory(directory=str(train_dir),
                                                      batch_size=BATCH_SIZE,
@@ -109,12 +113,12 @@ IMG_SHAPE = (IMG_HEIGHT, IMG_WIDTH, 3)
 model = Sequential([
     Conv2D(16, 3, padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH ,3)),
     MaxPooling2D(),
-    Dropout(0.2),
+    #Dropout(0.2),
     Conv2D(32, 3, padding='same', activation='relu'),
     MaxPooling2D(),
     Conv2D(64, 3, padding='same', activation='relu'),
     MaxPooling2D(),
-    Dropout(0.2),
+    #Dropout(0.2),
     Flatten(),
     Dense(512, activation='relu'),
     Dense(1, activation='sigmoid')
@@ -172,4 +176,51 @@ plt.plot(epochs_range, loss, label='Training Loss')
 plt.plot(epochs_range, val_loss, label='Validation Loss')
 plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
+plt.show()
+
+######################################
+# Launch the TEST
+######################################
+
+def get_test_data():
+    test_images = []
+    test_paths = []
+    for i in tqdm(os.listdir(test_dir)):
+        path = os.path.join(test_dir,i)
+        test_paths.append(path)
+        img = cv2.imread(path)
+        if str(type(img)).find('NoneType') != -1: 
+            print('test '+i+' is empty!')
+        else:
+            img = cv2.resize(img, (IMG_WIDTH,IMG_HEIGHT) ) 
+            test_images.append([np.array(img)])
+    return test_images, test_paths
+
+TEST_DATA, TEST_PATH = get_test_data()
+#displaying images
+fig=plt.figure(figsize=(14,14))
+
+for cnt, img in enumerate(TEST_DATA[:24]):
+    y = fig.add_subplot(6,5,cnt+1)
+    #data = img.reshape(1,239,100,1)
+    data = ts.cast(img, ts.float32) #cast data to the float32 format, the int8 being not compatible with tensorflow
+    model_out = model.predict([data])
+    argmax = np.argmax(model_out)
+    
+    print('---------------')
+    print(argmax)
+    argmax = round(argmax)
+    print(argmax)
+    argmax = int(argmax)
+    print(argmax)
+    
+    str_label = CLASS_NAMES[argmax]
+
+    #img = img[0].reshape(1,IMG_WIDTH,IMG_HEIGHT,1)
+    y.imshow(plt.imread(TEST_PATH[cnt]))
+
+    plt.title(str_label)
+    y.axes.get_xaxis().set_visible(False)
+    y.axes.get_yaxis().set_visible(False)
+
 plt.show()
